80a29f9b1048b37ec591a856beae8200b89a4ee7
jagades vijendran <jagadesvijendran0033@gmail.com>
2026-02-27 17:14:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -187,7 +187,7 @@ app.use("/api/login", loginroute);
 app.use("/api/analyze", analyzeRoute);
 app.use(express.urlencoded({ extended: true }));
 /* DB */
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
 .then(()=> console.log("Mongo Connected"))
 .catch(err=> console.log(err));
 
